f75f556396dedb8635aabb9dc03ef6f429c3c277
Olanrewaju Said <134246135+ol-s-cloud@users.noreply.github.com>
2025-02-15 05:09:20 -0800

    Initialize main package with core functionality

--- bitcoin_address_generator/__init__.py
+++ bitcoin_address_generator/__init__.py
@@ -1,3 +1,9 @@
+"""
+Bitcoin Address Generator Package
+
+A Python package for generating Bitcoin addresses using ECDSA
+"""
+
 from bitcoin_address_generator.core.keys import generate_private_key, get_public_key, get_compressed_public_key
 from bitcoin_address_generator.core.address import create_address
 
@@ -7,7 +13,9 @@ def generate_wallet(private_key=None):
     """Generate a new Bitcoin wallet or use existing private key."""
     if private_key is None:
         private_key = generate_private_key()
+    
     public_key = get_public_key(private_key)
     compressed_public_key = get_compressed_public_key(public_key)
     address = create_address(compressed_public_key)
+    
     return private_key, public_key, address
