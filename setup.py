af9cef5487334977186bd73589c58846ee02ddd2
Olanrewaju Said <134246135+ol-s-cloud@users.noreply.github.com>
2025-02-15 04:56:33 -0800

    Add setup.py with updated contact information

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="bitcoin-address-generator",
+    version="0.1.0",
+    author="ol-s-cloud",
+    author_email="gs_wl889@icloud.com",
+    description="A Python package for generating Bitcoin addresses using ECDSA",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ol-s-cloud/bitcoin-address-generator",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Security :: Cryptography",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "ecdsa>=0.18.0",
+        "base58>=2.1.1",
+        "click>=8.0.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "bitcoin-address-generator=bitcoin_address_generator.cli.main:generate_address",
+        ],
+    },
+)
